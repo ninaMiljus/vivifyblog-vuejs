59242cbac907720fef5ecada7345d208453af128
ninaMiljus <nina.miljus@gmail.com>
2021-05-30 15:26:07 +0200

    Zadatak 1

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+     <nav-bar></nav-bar>
+    <router-view></router-view>
   </div>
 </template>
+
+<script>
+import NavBar from './components/NavBar.vue'
+export default ({
+  name: 'app',
+  components: {
+    NavBar
+  }
+})
+</script>
 
 <style>
 #app {
